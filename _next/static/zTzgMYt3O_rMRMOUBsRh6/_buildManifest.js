5ec92a45120717a03af3616c5e7c43f0851b50b9
flock <qwer@example.com>
2025-11-30 19:44:28 +0530

    Updates

--- _next/static/zTzgMYt3O_rMRMOUBsRh6/_buildManifest.js
+++ _next/static/zTzgMYt3O_rMRMOUBsRh6/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/hack-rebels//_next/:path+",
+        "destination": "/hack-rebels/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
